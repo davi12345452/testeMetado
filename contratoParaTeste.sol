1caebf113869d82a7970192ae77a2634216500c4
Davi <88855441+davi12345452@users.noreply.github.com>
2023-05-22 08:47:31 -0300

    Create contratoParaTeste.sol
    contrato para testar alteração da URI na OpenSea

---
+++ contratoParaTeste.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SimpleNFT is ERC721, ERC721URIStorage, Ownable {
+    constructor() ERC721("SimpleNFT", "SNFT") {}
+
+    function mint(address to, uint256 tokenId, string memory tokenURI) public onlyOwner {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+    }
+
+    function setNewURI(uint256 tokenId, string memory tokenURI) public onlyOwner {
+        _setTokenURI(tokenId, tokenURI);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+}
